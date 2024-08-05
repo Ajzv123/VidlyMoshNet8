4b2c1c0dee4d1c4b4acb26e9cd7b4520286dc8a3
Aldo Joel Sanchez Benitez <95384676+Ajzv123@users.noreply.github.com>
2024-08-05 13:26:27 -0600

    Cambios listo migraciones
    Añadido
    - dbcontext.cs
    - lumen-boostrap.css
    - migrations
    Modificado
    - program.cs
    - layout.cshtml
    - appsettings.json

---
+++ VidlyMoshNet8/Data/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VidlyMoshNet8.Models;
+
+namespace VidlyMoshNet8.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Customers> Customers { get; set; }
+    }
+}
--- VidlyMoshNet8/Program.cs
+++ VidlyMoshNet8/Program.cs
@@ -1,9 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using VidlyMoshNet8.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+using (var scoppe = app.Services.CreateScope())
+{
+    var dataContext = scoppe.ServiceProvider.GetRequiredService<AppDbContext>();
+    dataContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
