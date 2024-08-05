079a52c1549a4ebdd887b8636d6a99735b427a36
Aldo Joel Sanchez Benitez <95384676+Ajzv123@users.noreply.github.com>
2024-08-05 17:14:30 -0600

    BirthDateCustomer added
    Cambios en el modelo de customer:
    - Se agrego Birthdate en el modelo
    Cambios en la vista de details:
    - Se agrego una lista para mostrar BirthDate y MembershipType
    Cambios en controlador de customers:
    - Se modifico la action Details, para manejar de forma adecuada el mermbershiptype

---
+++ VidlyMoshNet8/Migrations/20240805230742_AddBirthDatetoCustomer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VidlyMoshNet8.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBirthDatetoCustomer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birthdate",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthdate",
+                table: "Customers");
+        }
+    }
+}
--- VidlyMoshNet8/Models/Customers.cs
+++ VidlyMoshNet8/Models/Customers.cs
@@ -15,5 +15,6 @@ namespace VidlyMoshNet8.Models
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+        public DateTime? Birthdate { get; set; }
     }
 }
