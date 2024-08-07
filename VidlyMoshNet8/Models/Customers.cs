9697035b2b371f86fbcf14fbf184c9ca78631a58
Aldo Joel Sanchez Benitez <95384676+Ajzv123@users.noreply.github.com>
2024-08-07 15:14:05 -0600

    Cambios 1
    Se realizaron cambios en Site.css para resaltar mensajes
    Agregacion de validaciones personalizadas en Min18YearsIfAMember

--- VidlyMoshNet8/Models/Customers.cs
+++ VidlyMoshNet8/Models/Customers.cs
@@ -10,14 +10,20 @@ namespace VidlyMoshNet8.Models
     {
         public int Id { get; set; }
         
-        [Required]
+        [Required (ErrorMessage = "Por favor ingresa un nombre.")]
         [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
+        
         public MembershipType MembershipType { get; set; }
+        
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
-
+        
+        
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
+        
     }
 }
